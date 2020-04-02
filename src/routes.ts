478a2a81afb6de1d65e62765d083827c9f0fd2a3
will3g <willianpaulo_09@hotmail.com>
2020-04-02 20:35:07 -0300

    replace-route-method

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,6 @@ import UserController from './controllers/UserController';
 const routes = Router();
 
 routes.get('/list-users', UserController.index);
-routes.get('/send-email', UserController.create);
+routes.post('/send-email', UserController.create);
 
 export default routes;
